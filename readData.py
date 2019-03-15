4a66bd485f269aabe6ccf8d2b0d9aa2ad6f4d417
swiftjab <48166604+swiftjab@users.noreply.github.com>
2019-03-15 15:01:39 -0700

    Update readData.py

--- readData.py
+++ readData.py
@@ -3,9 +3,15 @@ import pandas as pd
 import sklearn as sk
 
 
+#df = pd.read_csv("H1B_dataset.csv", encoding='latin-1', low_memory=False, usecols=['CASE_SUBMITTED_YEAR','DECISION_YEAR', 'EMPLOYER_NAME', 'EMPLOYER_STATE', 'SOC_NAME', 'PREVAILING_WAGE', 'CASE_STATUS'])
 
-df = pd.read_csv("H1B_dataset.csv", encoding='latin-1', low_memory=False, usecols=['CASE_SUBMITTED_YEAR','DECISION_YEAR', 'EMPLOYER_NAME', 'EMPLOYER_STATE', 'SOC_NAME', 'PREVAILING_WAGE', 'CASE_STATUS'])
+df = pd.read_csv("totalnew.csv", encoding ='latin-1', low_memory = False)
 
+totaldf = df.reset_index()
 df = df[df.CASE_SUBMITTED_YEAR.isin(range(2014,2019))]
+df = pd.read_csv('totalnew.csv',low_memory=False)
+totaldfn = df.reset_index()
+df = totaldfn.drop(['index'], axis=1)
+
 
 print(df)
