77e7af222df62b9a6fed1f4da2bdd1de2e9bbd5f
swiftjab <48166604+swiftjab@users.noreply.github.com>
2019-03-14 17:12:25 -0700

    Update readData.py

--- readData.py
+++ readData.py
@@ -6,6 +6,6 @@ import sklearn as sk
 
 df = pd.read_csv("H1B_dataset.csv", encoding='latin-1', low_memory=False, usecols=['CASE_SUBMITTED_YEAR','DECISION_YEAR', 'EMPLOYER_NAME', 'EMPLOYER_STATE', 'SOC_NAME', 'PREVAILING_WAGE', 'CASE_STATUS'])
 
-df = df[df.CASE_SUBMITTED_YEAR.isin(range(2014,2018))]
+df = df[df.CASE_SUBMITTED_YEAR.isin(range(2014,2019))]
 
 print(df)
